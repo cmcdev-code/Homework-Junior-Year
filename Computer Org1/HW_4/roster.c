dbae272f08a24f3012479911eca65638c5b7b948
Collin McDevitt <cmcdev@vt.edu>
2024-03-24 14:33:55 -0400

    Cooking

--- Computer Org1/HW_4/roster.c
+++ Computer Org1/HW_4/roster.c
@@ -1,6 +1,7 @@
 #include "roster.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_STUDENT_NAME_LEN 100 
 int add_student(FILE* in, FILE* out, char* students[], int current_students, int const max_students){
@@ -13,7 +14,7 @@ int add_student(FILE* in, FILE* out, char* students[], int current_students, int
     }
 
     students[current_students] = name_buffer;
-    fprintf(out, "Command: add %s\n    %s was added. %d spot(s) remain", name_buffer,name_buffer,max_students-current_students-1);
+    fprintf(out, "Command: add %s\n    %s was added. %d spot(s) remain\n", name_buffer,name_buffer,max_students-current_students-1);
     return current_students+1;
 }
 
@@ -26,16 +27,43 @@ void modify_name(FILE* in, FILE* out, char* students[], int current_students){
         if(strcmp(students[i],old_name_buffer) == 0){
             free(students[i]);
             students[i] = new_name_buffer;
-            fprintf(out, "Command: modify %s:%s\n    %s name modified to %s.", old_name_buffer,new_name_buffer,old_name_buffer,new_name_buffer);
+            fprintf(out, "Command: modify %s:%s\n    %s name modified to %s.\n", old_name_buffer,new_name_buffer,old_name_buffer,new_name_buffer);
             free(old_name_buffer);
             return;
         }
     }
     //Case 2 the student was not found
-    fprintf(out,"Command: modify %s:%s\n    No student with name %s found.", old_name_buffer,new_name_buffer,old_name_buffer);
+    fprintf(out,"Command: modify %s:%s\n    No student with name %s found.\n", old_name_buffer,new_name_buffer,old_name_buffer);
     free(old_name_buffer);
     free(new_name_buffer);
 }
+
+int remove_student(FILE* in, FILE* out, char* students[], int current_students, int const max_students){
+    char *name_buffer = (char *)malloc(sizeof(char)*MAX_STUDENT_NAME_LEN);
+    fscanf(in, "%99[^\n]\n",name_buffer);
+    //Case 1 the student was found
+    for(int i = 0; i< current_students;++i){
+        if(strcmp(students[i],name_buffer)==0){
+            free(students[i]);
+            for(int j = i ; j<current_students-1;++j){
+                students[j] = students[j+1];
+            }
+            //Setting last student to null
+            students[--current_students] = NULL;
+            
+            fprintf(out, "Command: remove %s\n    Student %s removed. The course now has %d seats remaining.\n", name_buffer,name_buffer,max_students-current_students);
+            free(name_buffer);
+            return current_students;
+        }
+    }
+    // //Case 2 the student was not found 
+    // Command: remove Siwei Cao
+    // No student named Siwei Cao was found to remove.
+    fprintf(out,"Command: remove %s\n    No student named %s was found to remove.\n", name_buffer,name_buffer);
+    free(name_buffer);
+    return current_students;
+}
+
 
 
 
@@ -51,6 +79,10 @@ int main(void){
     int current_students = 0;
 
     current_students = add_student(in, out, students, current_students, max_students);
+    printf("\n %s \n",students[0]);
+    modify_name(in, out, students, current_students);
+        printf("\n %s \n",students[0]);
+
     fclose(in);
     fclose(out);
     free(students);
